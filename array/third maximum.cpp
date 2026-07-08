bd9e73a4d219cebfc83584a781aa506ed2909f68
LOKESH <prajapatilokesh158@gmail.com>
2026-07-08 22:36:07 +0530

    Create third maximum.cpp

---
+++ array/third maximum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long first=LLONG_MIN;
+        long long second=LLONG_MIN;
+        long long third=LLONG_MIN;
+        
+        for(long long  i:nums){
+            if(i==first || i==second || i==third) continue;
+            if(i>first){
+               
+               
+                third=second;
+                 second=first;
+                 first=i;
+            }
+            else if(i>second){
+                
+                third=second;
+                second =i;
+            }
+            else if(i>third){
+                third=i;
+               
+            }
+        }
+          if(third==LLONG_MIN ) return first;
+        // if(second==INT_MIN) return first;
+      
+        return third;
+
+    }
+};
